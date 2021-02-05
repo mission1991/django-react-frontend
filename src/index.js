b26cc8541ac9744476373cbcf15f64818e2279ea
mission1991 <missionfactory91@gmail.com>
2021-02-05 14:34:50 +0900

    api 수정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+import { AppProvider } from "store";
+import Root from "./pages";
+import "antd/dist/antd.css";
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <AppProvider>
+      <Root />
+    </AppProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
